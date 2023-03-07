3596415efabcb31f51ba53c7b6efaa450eebd298
Hana Kim <hana@getroutes.co>
2023-03-07 12:32:27 -0800

    Initial Commit

---
+++ Routes/RoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutesApp.swift
+//  Routes
+//
+//  Created by Hana Kim on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
